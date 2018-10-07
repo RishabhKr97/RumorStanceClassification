69dadd90abe6d66dd9561c72cb72e5ccc4b57a4a
penguin2048 <rishabhkr1729@gmail.com>
2018-10-07 18:55:20 +0530

    Improve code structure and update field names

--- DataProcessingScripts/twitter_data_duplicate_removal.py
+++ DataProcessingScripts/twitter_data_duplicate_removal.py
@@ -1,37 +1,37 @@
 # REMOVE DUPLICATE TWEETS FROM DATA
 import csv
 
-# CHANGE ONLY SOURCEFILE
-SOURCEFILE = 'airfrance.csv'
-
-SOURCEPATH = '../Data/' + SOURCEFILE
-TARGETPATH = '../Data/' + SOURCEFILE[:-3] + 'csv'
+FILES = ['airfrance', 'cell', 'michelle', 'obama', 'palin']
 FIELDS = ['tweet', 'number_of_mentions', 'number_of_urls', 'classification']
 
-# OPEN SOURCE FILE AND TARGET FILE
-source_file = open(SOURCEPATH, 'r', newline='', encoding='utf-8', errors='ignore')
-source_csv = csv.reader(source_file, delimiter=',')
-source_csv = list(source_csv)
-source_index = 0
-source_file.close()
-target_file = open(TARGETPATH, 'w', newline='', encoding='utf-8')
-target_csv = csv.DictWriter(target_file, FIELDS)
+for file_name in FILES:
+    # OPEN SOURCE FILE AND TARGET FILE
+    source_path = '../Data/' + file_name + '.csv'
+    target_path = '../Data/' + file_name + '.csv'
+    source_file = open(source_path, 'r', newline='', encoding='utf-8')
+    source_csv = csv.reader(source_file, delimiter=',')
+    # SKIP HEADERS
+    next(source_csv)
+    source_csv = list(source_csv)
+    source_file.close()
+    target_file = open(target_path, 'w', newline='', encoding='utf-8')
+    target_csv = csv.DictWriter(target_file, FIELDS)
+    target_csv.writeheader()
 
-print("LENGTH BEFORE: {}".format(len(source_csv)))
-total = 0
-unique_tweets = set()
-# CONSTRUCT UNIQUE OBJECT AND SAVE TO CSV
-while source_index < len(source_csv):
-    if source_csv[source_index][2] not in unique_tweets:
-        obj = {}
-        obj['tweet'] = source_csv[source_index][0]
-        obj['number_of_mentions'] = source_csv[source_index][1]
-        obj['number_of_urls'] = source_csv[source_index][2]
-        obj['classification'] = source_csv[source_index][3]
-        target_csv.writerow(obj)
-        unique_tweets.add(obj['tweet'])
-        total += 1
-    source_index += 1
+    print("NUMBER OF TWEETS IN {}.csv = {}".format(file_name, len(source_csv)))
+    count = 0
+    unique_tweets = set()
+    # CONSTRUCT UNIQUE OBJECT AND SAVE TO CSV
+    for tweet in source_csv:
+        if tweet[0] not in unique_tweets:
+            obj = {}
+            obj['tweet'] = tweet[0]
+            obj['number_of_mentions'] = tweet[1]
+            obj['number_of_urls'] = tweet[2]
+            obj['classification'] = tweet[3]
+            target_csv.writerow(obj)
+            unique_tweets.add(obj['tweet'])
+            count += 1
 
-print("LENGTH AFTER: {}".format(total))
-target_file.close()
+    print("NUMBER OF TWEETS IN OUTPUT {}.csv = {}".format(file_name, count))
+    target_file.close()
