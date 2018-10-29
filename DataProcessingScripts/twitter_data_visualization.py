735b26044162c6ff48842bc8c5b5d82196eda85d
penguin2048 <rishabhkr1729@gmail.com>
2018-10-29 19:19:37 +0530

    Add pie chart for visualization of words with highest frequencies

--- DataProcessingScripts/twitter_data_visualization.py
+++ DataProcessingScripts/twitter_data_visualization.py
@@ -5,11 +5,13 @@ import itertools
 import numpy as np
 import matplotlib.pyplot as plt
 import re
+from collections import Counter
 
 '''
     VISUALIZE USING
     1) Histogram of number of words for each classification
     2) Bar graph between classification and number of tweets
+    3) Words with highest frequency
 '''
 
 FILES = ['airfrance', 'cell', 'michelle', 'obama', 'palin']
@@ -20,8 +22,9 @@ def main():
     choice = int(input("""
     1) ENTER 1 FOR HISTOGRAM OF WORD COUNT IN TWEET FOR EACH CLASSIFICATION.
     2) ENTER 2 FOR BAR GRAPH OF CLASSIFICATION VS NUMBER OF TWEETS.
-    3) ENTER 3 FOR BOTH.
-    4) ENTER 4 FOR OVERALL STATS ONLY.
+    3) ENTER 3 FOR PIE CHART OF HIGHEST WORD FREQUENCY FOR EACH CLASSIFICATION.
+    4) ENTER 4 FOR HISTOGRAM OF WORD NUMBER FEATURES FOR EACH FILE.
+    5) ENTER 5 FOR ALL OVERALL STATS.
     """))
 
     if choice == 1:
@@ -29,16 +32,20 @@ def main():
     elif choice == 2:
         bar_graph()
     elif choice == 3:
-        histogram()
-        bar_graph()
+        word_frequency()
     elif choice == 4:
-        histogram(overall_only = True)
-        bar_graph(overall_only = True)
+        # word_number_feature()
+        pass
+    elif choice == 5:
+        histogram(overall_only=True)
+        bar_graph(overall_only=True)
+        word_frequency(overall_only=True)
+        # word_number_feature(overall_only=True)
     else:
         print("WRONG CHOICE!!")
         exit()
 
-# HISTOGRAM OF TWEET LENGTH FOR EACH CLASSIFICATION IN EANCH FILE AND OVERALL DATASET
+# HISTOGRAM OF TWEET LENGTH FOR EACH CLASSIFICATION IN EACH FILE AND OVERALL DATASET
 def histogram(overall_only = False):
     overall_numbers = [[] for _ in range(4)]
     for file_name in FILES:
@@ -50,7 +57,7 @@ def histogram(overall_only = False):
 
         numbers = [[] for _ in range(4)]
         for tweet in source_csv:
-            numbers[int(tweet[5])-1].append(len(re.findall(r'\w+', tweet[0])))
+            numbers[int(tweet[9])-1].append(len(re.findall(r'\w+', tweet[0])))
 
         for i in range(4):
             overall_numbers[i] += numbers[i]
@@ -84,7 +91,7 @@ def bar_graph(overall_only = False):
 
         numbers = [0]*4
         for tweet in source_csv:
-            numbers[int(tweet[5])-1] += 1
+            numbers[int(tweet[9])-1] += 1
         for j in range(4):
             overall_numbers[j] += numbers[j]
         if not overall_only:
@@ -104,5 +111,56 @@ def bar_graph(overall_only = False):
     plt.ylabel("number of tweets")
     plt.xticks(np.arange(4), CLASSIFICATION, rotation=30)
     plt.show()
+
+# PIE CHART OF WORDS WITH HIGHEST FREQUENCY IN EACH CLASS IN EACH FILE AND OVERALL DATASET
+def word_frequency(overall_only = False):
+    TOP_N = 20
+    overall_words = [{}, {}, {}, {}]
+    for file_name in FILES:
+        source_path = '../Data/' + file_name + '.csv'
+        source_file = open(source_path, 'r', newline='', encoding='utf-8')
+        source_csv = csv.reader(source_file, delimiter=',')
+        # SKIP HEADERS
+        next(source_csv)
+
+        words = [{}, {}, {}, {}]
+        for tweet in source_csv:
+            for word in tweet[0].split():
+                if word not in words[int(tweet[9])-1]:
+                    words[int(tweet[9])-1][word] = 1
+                else:
+                    words[int(tweet[9])-1][word] += 1
+
+                if word not in overall_words[int(tweet[9])-1]:
+                    overall_words[int(tweet[9])-1][word] = 1
+                else:
+                    overall_words[int(tweet[9])-1][word] += 1
+
+        if not overall_only:
+            for i in range(4):
+                top_n = min(TOP_N, len(words[i]))
+                stat_list = Counter(words[i]).most_common(top_n)
+                word = []
+                count = []
+                for j in range(top_n):
+                    word.append(stat_list[j][0])
+                    count.append(stat_list[j][1])
+
+                plt.pie(count, labels=word, colors=COLORS, counterclock=False, startangle=90)
+                plt.title(file_name + " : " + CLASSIFICATION[i])
+                plt.show()
+
+    for i in range(4):
+        top_n = min(TOP_N, len(words[i]))
+        stat_list = Counter(overall_words[i]).most_common(top_n)
+        word = []
+        count = []
+        for j in range(top_n):
+            word.append(stat_list[j][0])
+            count.append(stat_list[j][1])
+
+        plt.pie(count, labels=word, colors=COLORS, counterclock=False, startangle=90)
+        plt.title("OVERALL DATASET : " + CLASSIFICATION[i])
+        plt.show()
 
 main()
