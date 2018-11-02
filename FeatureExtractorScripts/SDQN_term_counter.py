1a85ac52563f000db8c1f3240e556fd27149b63b
penguin2048 <rishabhkr1729@gmail.com>
2018-11-02 17:06:58 +0530

    Remove Negation word column and Add more query words

--- FeatureExtractorScripts/SDQN_term_counter.py
+++ FeatureExtractorScripts/SDQN_term_counter.py
@@ -2,13 +2,13 @@
 
 import csv
 
-SUPPORT_WORDS = ['true', 'exactly', 'yes', 'indeed', 'omg', 'know']
-DENIAL_WORDS = ['not true', 'don\'t agree','dont agree', 'impossible', 'false', 'shut', 'rumors', 'Rumor']
-QUERY_WORDS = ['what', 'why', 'how', 'when', 'where', 'did', 'do', 'does', 'have', 'has', 'Is', 'Are', 'Can', 'could', 'may', 'would', 'will']
-NEGATION_WORDS =  ['not', 'no', 'nobody', 'nothing', 'none', 'never', 'neither', 'nor', 'nowhere', 'hardly', 'scarcely', 'barely', 'don\'t', 'isn\'t', 'wasn\'t', 'shouldn\'t', 'wouldn\'t', 'couldn\'t', 'doesn\'t', 'dont', 'isnt', 'wasnt', 'shouldnt', 'wouldnt', 'couldnt', 'doesnt']
+SUPPORT_WORDS = ['true', 'exactly', 'yes', 'indeed', 'omg', 'know', 'agree', 'perfect', 'yeah']
+DENIAL_WORDS = ['not', 'no', 'nobody', 'nothing', 'none', 'never', 'neither', 'nor', 'nowhere', 'hardly', 'scarcely', 'barely', 'impossible', 'false', 'shut', 'rumors', 'rumor']
+QUERY_WORDS = ['would', 'doesnt', 'do', "hadn't", 'dont', "shouldn't", 'wouldnt', 'have', "who's", "haven't", "don't", 'has', 'will', 'why', 'shouldnt', 'can', 'Is', 'did', "aren't", "doesn't", 'isnt', 'what', 'does', "hasn't", "wasn't", 'when', 'how', 'are', "wouldn't", 'may', "where's", "why's", 'could', "what's", "couldn't", 'couldnt', "isn't", 'wasnt', "weren't", 'where']
+
 
 FILES = ['airfrance', 'cell', 'michelle', 'obama', 'palin']
-FIELDS = ['tweet', 'number_of_mentions', 'number_of_urls', 'number_of_question_marks', 'number_of_exclaimation_marks', 'number_of_support_words', 'number_of_denial_words', 'number_of_query_words', 'number_of_negation_words', 'classification']
+FIELDS = ['tweet', 'number_of_mentions', 'number_of_urls', 'number_of_question_marks', 'number_of_exclaimation_marks', 'number_of_support_words', 'number_of_denial_words', 'number_of_query_words', 'classification']
 
 for file_name in FILES:
     # OPEN SOURCE FILE AND TARGET FILE
@@ -25,8 +25,8 @@ for file_name in FILES:
     target_csv.writeheader()
 
     print("NUMBER OF TWEETS IN {}.csv = {}".format(file_name, len(source_csv)))
-    count = 0
     # ADD COUNTS AS COLUMNS
+    count = 0
     for tweet in source_csv:
         obj = {}
         obj['tweet'] = tweet[0]
@@ -34,13 +34,12 @@ for file_name in FILES:
         obj['number_of_urls'] = tweet[2]
         obj['number_of_question_marks'] = tweet[3]
         obj['number_of_exclaimation_marks'] = tweet[4]
-        obj['number_of_support_words'] = len([w for w in tweet[0].split() if w in SUPPORT_WORDS])
-        obj['number_of_denial_words'] = len([w for w in tweet[0].split() if w in DENIAL_WORDS])
-        obj['number_of_query_words'] = len([w for w in tweet[0].split() if w in QUERY_WORDS])
-        obj['number_of_negation_words'] = len([w for w in tweet[0].split() if w in NEGATION_WORDS])
+        obj['number_of_support_words'] = len([w for w in tweet[0].split() if w.lower() in SUPPORT_WORDS])
+        obj['number_of_denial_words'] = len([w for w in tweet[0].split() if w.lower() in DENIAL_WORDS])
+        obj['number_of_query_words'] = len([w for w in tweet[0].split() if (w.lower() in QUERY_WORDS or w == "Is")])
         obj['classification'] = tweet[5]
         target_csv.writerow(obj)
         count += 1
 
-    print("NUMBER OF TWEETS IN OUTPUT {}.csv = {}".format(file_name, count))
+    print("NUMBER OF TWEETS IN OUTPUT {}.csv = {}".format(file_name, len(source_csv)))
     target_file.close()
